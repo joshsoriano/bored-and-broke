64b5886049be343a03e239506362320033c91611
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2017-10-15 18:11:48 -0700

    getting rid of tuttorial files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Welcome to:';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          <span>
+          Bored and Broke!
+          </span>
+        </div>
+        <div className="new-title">
+          <span>This is our project</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
