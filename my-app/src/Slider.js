ef18e1b17f02246062ab6205b78e80767f3c0434
Megan Karbowski <mck064@gmail.com>
2017-10-22 16:04:21 -0700

    started the form for getting the radius picked by the user

--- my-app/src/Slider.js
+++ my-app/src/Slider.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
 class Slider extends React.Component {
-    render() {
-    return (
-        <div id="slidecontainer">
-            <form>
-                <input type="range" min="1" max="100" id="myRange"></input>
-            </form>
-        </div>
-    );
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+        console.log(this.state.value);
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefualt();
+    }
+
+    render() {
+        return (
+            <div id="slidecontainer">
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    <input type="range" min="1" max="100" id="radius" value={this.state.value} onChange={this.handleChange} /> {this.state.value} miles</label>
+                </form>
+            </div>
+        );
+    }
+
 }
 export default Slider;
