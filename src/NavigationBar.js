ade17fd0bff041a8e04ed42e613196a3e76b6c7b
Josh Soriano <joshsoriano@Joshs-MacBook-Pro.local>
2017-11-06 15:23:34 -0800

    fixed spacing on NavigationBar.js

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -11,26 +11,26 @@ import logo from './images/logo-white.png'
 class NavigationBar extends React.Component {
     render() {
         return(
-          <Navbar inverse>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="#">
-                    <img className="logo" src={logo} width="150px" alt="user pic" />
-                </a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-              <Nav pullRight>
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                  <MenuItem eventKey={3.1}>Settings</MenuItem>
-                  <MenuItem eventKey={3.2}>My Saved Activities</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey={3.3}>Log Out</MenuItem>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">
+                            <img className="logo" src={logo} width="150px" alt="user pic" />
+                        </a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Settings</MenuItem>
+                            <MenuItem eventKey={3.2}>My Saved Activities</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Log Out</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 };
