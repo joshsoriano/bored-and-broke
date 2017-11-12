3cd6c3aa93fd2d852db4dd40a331f6e3d8e54464
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2017-11-12 13:15:30 -0800

    linking the pages

--- src/Settings.js
+++ src/Settings.js
@@ -5,7 +5,6 @@ import Carousel from 'react-bootstrap/lib/Carousel';
 import Button from 'react-bootstrap/lib/Button';
 import {Form, FormGroup, ControlLabel, FormControl, Col, Checkbox} from 'react-bootstrap'
 import eventExample from './eventExample.png';
-import logo from './logo.png';
 import NavigationBar from './NavigationBar.js';
 import logo_offwhite from './images/logo-offwhite.png';
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import injectSheet from 'react-jss';
 // import './index.css';
 import PropTypes from 'prop-types';
 import Homepage from './Homepage.js';
-// import LoginCreateAccount from './LoginCreateAccount.js';
-// import SavedActivities from './SavedActivities.js';
-// import Settings from './Settings.js';
+import LoginCreateAccount from './LoginCreateAccount.js';
+import SavedActivities from './SavedActivities.js';
+import Settings from './Settings.js';
 // import SingleActivityModal from './SingleActivityModal.js';
 
 
@@ -23,17 +24,60 @@ const styles = {
     },
 };
 
+// const Test = () => (
+//     <Router>
+//         <div>
+//             <ul>
+//                 <li><Link to="/Homepage">Homepage</Link></li>
+//                 <li><Link to="/SavedActivities">SavedActivities</Link></li>
+//                 <li><Link to="/Settings">Settings</Link></li>
+//                 <li><Link to="/LoginCreateAccount">LoginCreateAccount</Link></li>
+//             </ul>
+//
+//             <hr />
+//
+//             <Route exact path="/" component={Homepage}/>
+//             <Route path="/about" component={SavedActivities}/>
+//             <Route path="/topics" component={Settings}/>
+//             <Route path="/LoginCreateAccount" component={LoginCreateAccount}/>
+//         </div>
+//     </Router>
+// )
+
+// const TestTopics = ({ match }) => (
+//     <div>
+//         <h2
+// )
+
 class BoredAndBroke extends React.Component {
   render() {
     // const status = 'This is our project:';
     const { classes } = this.props;
     return (
-          <div className={ classes.main }>
-            <div>
-                <Homepage />
-            </div>
+        //   <div className={ classes.main }>
+        //     <div>
+        //         <Homepage />
+        //     </div>
+         //
+        //  </div>
+         <Router>
+             <div>
+                 <ul>
+                     <li><Link to="/Homepage">Homepage</Link></li>
+                     <li><Link to="/SavedActivities">SavedActivities</Link></li>
+                     <li><Link to="/Settings">Settings</Link></li>
+                     <li><Link to="/LoginCreateAccount">LoginCreateAccount</Link></li>
+                 </ul>
 
-         </div>
+                 <hr />
+
+                //  <Route exact path="./" component={Homepage}/>
+                 <Route path="/Homepage" component={Homepage}/>
+                 <Route path="/SavedActivities" component={SavedActivities}/>
+                 <Route path="/Settings" component={Settings}/>
+                 <Route path="/LoginCreateAccount" component={LoginCreateAccount}/>
+             </div>
+         </Router>
       );
     }
   }
