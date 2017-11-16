b01da874501c7a390a72a119615ce44d9c996bc2
maryalverson <malverso@lion.lmu.edu>
2017-11-15 17:13:12 -0800

    Wrote database queries.

--- src/backend/database.js
+++ src/backend/database.js
@@ -15,24 +15,63 @@ module.exports = (() => {
     return {
           getActivity: (activityId) => {
             // Return activity object from the database.
+            Models.Activity.findOne({
+              where: {
+                id: activityId
+              }
+            });
+            // TODO: parse into Activity object.
             return {};
           },
 
           getFutureActivities: (priceLimit, location="Los Angeles") => {
             // Return an array of activities that occur in the future from the database.
-
-            // DEMO PURPOSES ONLY!!!
-            return activityRetriever.retrieve();
+            return [];
           },
 
           getSavedActivities: (userId) => {
             // Return an array of activities that have been saved by a user.
+            Models.Tagline.findAll({
+              where: {
+                user_id: userId
+              }
+            });
+
+            // Parse into activity objects.
             return [];
+          },
+
+          getTagline: (userId, activityId) => {
+            // Get a user's tagline for an event.
+            let tagline = Models.Tagline.findOne({
+              attributes: ['tagline'],
+              where: {
+                user_id: userId,
+                activity_id: activityId
+              }
+            });
+            return tagline;
           },
 
           saveActivity: (userId, activityId, tagline) => {
             // Store userId, activityId, and tagline in the database.
+            // First, remove all single quotes. Single quotes will mess up PostGres.
+            let parsedTag = tagline.replace("'", "");
+            Models.Tagline.create({
+              user_id: userId,
+              activty_id: activityId,
+              tagline: parsedTag
+            });
+          },
 
+          unsaveActivity: (userId, activityId) => {
+            // Remove tagline row for this userId and activityId in the database.
+            Models.Tagline.destroy({
+              where: {
+                user_id: userId,
+                activity_id: activityId
+              }
+            });
           },
 
           addActivities: (location="Los Angeles") => {
@@ -58,11 +97,26 @@ module.exports = (() => {
             return false;
           },
 
+          removeUser: (userId) => {
+            // Remove a user from the Users table of the database.
+            Models.User.destroy({
+              where: {
+                id: userId
+              }
+            });
+
+            // Also remove their taglines.
+            Models.Tagline.destroy({
+              where: {
+                user_id: userId
+              }
+            });
+          },
+
           sync: (sequelize) => {
             // Creates the tables according to the models defined in models.js
             // if they do not exist already.
             Models.sync(sequelize);
-
           }
     };
 })();
