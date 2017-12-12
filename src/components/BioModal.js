9e671d4f2956477e452c9d70febfe99322dc1b02
jordan-woods <jordan.k.woods@gmail.com>
2017-12-12 11:22:44 -0800

    Update formatting and include top comment

--- src/components/BioModal.js
+++ src/components/BioModal.js
@@ -1,3 +1,7 @@
+/**
+    This file controls the modal that pops up the first time a user logs into Bored&Broke. It requests that the user create a bio.
+**/
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import injectSheet from 'react-jss';
@@ -11,92 +15,68 @@ const propTypes = {
 };
 
 const styles = {
-  bioInput: {
-      fontFamily: 'Open Sans',
-  },
-  buttonContainer: {
-      marginTop: '15px',
-      display: 'flex',
-      justifyContent: 'center',
-      marginBottom: '15px',
-      fontFamily: 'Open Sans',
-  }
+    bioInput: {
+        fontFamily: 'Open Sans',
+    },
+    buttonContainer: {
+        marginTop: '15px',
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '15px',
+        fontFamily: 'Open Sans',
+    }
 };
 
 const userID = getUserID();
 const userName = getUserName();
 
 class BioModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleBio = this.handleBio.bind(this);
-    // this.shouldShow = this.shouldShow.bind(this);
-    this.state = {
-      value: '',
-      showModal: this.props.is_first_time
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.handleBio = this.handleBio.bind(this);
+        this.state = {
+            value: '',
+            showModal: this.props.is_first_time
+        };
+    }
 
-  // componentWillMount() {
-  //   const userID = getUserID();
-  //   const userName = getUserName();
-  //   Promise.resolve(this.props.actions.findOrCreateUser(userID, userName)).then(function() {
-  //     this.setState({
-  //       showModal: this.props.is_first_time
-  //     });
-  //     console.log(this.props.is_first_time);
-  //   });
-  // }
+    close = () => {
+        this.setState({ showModal: false });
+        this.props.actions.saveUserSettings(this.state.value, userID);
+        this.props.actions.getUser(userID);
+        this.props.actions.findOrCreateUser(userID, userName);
+    }
 
-  // shouldShow() {
-  //
-  // }
+    handleBio = (e) => {
+        this.setState({
+            value: e.target.value,
+        });
+    }
 
-  close = () => {
-    this.setState({ showModal: false });
-    console.log(this.state.value);
-    this.props.actions.saveUserSettings(this.state.value, userID);
-    this.props.actions.getUser(userID);
-    this.props.actions.findOrCreateUser(userID, userName);
-  }
-
-  // getBioState = () => {
-  //     const taglineVal = this.state.value;  //this is the most accurate one! Use this!
-  //     console.log("taglineVal:", taglineVal)
-  //     return taglineVal;
-  // };
-
-  handleBio = (e) => {
-    this.setState({
-        value: e.target.value, //note that the taglineVal is more accurate
-    });
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header closeButton>
-            <Modal.Title>Welcome to Bored&Broke!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-              <h5>Create your bio! This will be visible to everyone who saves the same activities as you.</h5>
-              <div className={ classes.bioInput }>
-                  <FormGroup controlId="formControlsTextarea">
-                    <ControlLabel></ControlLabel>
-                    <FormControl componentClass="textarea" value={ this.state.value } placeholder='Describe yourself!' onChange={ this.handleBio }/>
-                  </FormGroup>
-              </div>
-
-              <div className={ classes.buttonContainer }>
-                <Button onClick={ this.close }>Save</Button>
-              </div>
-          </Modal.Body>
-        </Modal>
-      </div>
-    );
-  }
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <Modal show={this.state.showModal} onHide={this.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Welcome to Bored&Broke!</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h5>Create your bio! This will be visible to everyone who saves the same activities as you.</h5>
+                        <div className={ classes.bioInput }>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel></ControlLabel>
+                                <FormControl componentClass="textarea" value={ this.state.value } placeholder='Describe yourself!' onChange={ this.handleBio }/>
+                            </FormGroup>
+                        </div>
+                        <div className={ classes.buttonContainer }>
+                            <Button onClick={ this.close }>Save</Button>
+                        </div>
+                    </Modal.Body>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 BioModal.propTypes = propTypes;
